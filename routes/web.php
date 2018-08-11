d5235460f29cf5f3aaac47b1e2ec67df4396a0e1
Quan Nguyen <quannguyen1456@gmail.com>
2018-08-11 23:09:09 +0700

    master: Bài 4: Tìm Hiểu Về Route Cơ Bản Trong Laravel 5.x - Lập Trình Laravel Khoa Phạm

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ Route::get('/', function () {
 });
 
 Route::get('thongtin', 'WelcomController@showinfo');
+Route::get('thongtin/{monhoc}/{thoigian}', function ($monhoc, $thoigian){
+    return 'Day la khoa hoc lap trinh ' . $monhoc . ' '  . $thoigian;
+});
+
+Route::get('mon-an/{mon_an?}', function ($mon_an = 'KFC'){
+    return $mon_an;
+});
+
+Route::get('thong-tin/{hoten}/{sodienthoai}', function ($hoten, $sodienthoai){
+    return $hoten . ' ' . $sodienthoai;
+})->where(['hoten' => '[a-zA-Z]+', 'sodienthoai' => '[0-9]{9,10}']);
